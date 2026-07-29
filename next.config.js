92ffec21a2918d587090b2e079908085c89e416d
edataworker <edataworker@gmail.com>
2026-07-29 15:21:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Remove experimental.appDir - it's deprecated
   
+  // Optional: Add path aliases
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': __dirname,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
